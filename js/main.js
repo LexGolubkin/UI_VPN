77049af2565b10c329332d0522fdbeca96b3ae65
lex_golubkin <aalexandr.golubkin@gmail.com>
2024-12-04 13:48:43 +0400

    request console output added

--- js/main.js
+++ js/main.js
@@ -17,50 +17,53 @@ tg.expand();
 
 
 window.addEventListener("load", () => {
-    let user_query = tg.initData;       // string is URL-parameters
+    let user_query = tg.initData;
+    console.log(user_query);       // string is URL-parameters
+
     if (user_query == null) {
         let sect = document.querySelector("#main_section");
         let hi = document.createElement("p");
         hi.textContent = 'Привет новый пользователь.';
         sect.appendChild(hi);
         return 0
-    } 
-    let params = new URLSearchParams(user_query);
-    
-    // Get query params
-    let user = JSON.parse(decodeURIComponent(params.get('user')));
-    user_id = user.id;
-    username = user.username;
-    user_photo = user.photo_url;
-    user_language = user.language_code;
+    } else {
+        let params = new URLSearchParams(user_query);
+        
+        // Get query params
+        let user = JSON.parse(decodeURIComponent(params.get('user')));
+        user_id = user.id;
+        username = user.username;
+        user_photo = user.photo_url;
+        user_language = user.language_code;
 
-    // Output Customer info if is a customer
-    let sect = document.querySelector("#main_section");
-    let img = document.createElement("img");
-    img.src = user_photo;
-    img.height = "100px";
-    img.width = "100px";
-    img.radius = "50px";
-    sect.appendChild(img);
+        // Output Customer info if is a customer
+        let sect = document.querySelector("#main_section");
+        let img = document.createElement("img");
+        img.src = user_photo;
+        img.height = "100px";
+        img.width = "100px";
+        img.radius = "50px";
+        sect.appendChild(img);
 
-    let user_info_id = document.createElement("h4");
-    user_info_id.textContent = "User ID";
-    sect.appendChild(user_info_id);
-    user_info_id.appendChild(user_id);
+        let user_info_id = document.createElement("h4");
+        user_info_id.textContent = "User ID";
+        sect.appendChild(user_info_id);
+        user_info_id.appendChild(user_id);
 
-    let user_info_username = document.createElement("h4");
-    user_info_username.textContent = "Username (tg)";
-    sect.appendChild(user_info_username);
-    user_info_username.appendChild(username);
+        let user_info_username = document.createElement("h4");
+        user_info_username.textContent = "Username (tg)";
+        sect.appendChild(user_info_username);
+        user_info_username.appendChild(username);
 
-    let user_info_lang = document.createElement("h4");
-    user_info_lang.textContent = "User language";
-    sect.appendChild(user_info_lang);
-    user_info_lang.appendChild(user_language);
+        let user_info_lang = document.createElement("h4");
+        user_info_lang.textContent = "User language";
+        sect.appendChild(user_info_lang);
+        user_info_lang.appendChild(user_language);
 
-    // document.querySelector('#user_id').textContent = user_id;
-    // document.querySelector('#username').innerText = username;
-    // document.querySelector('#lang').innerText = user_language;
+        // document.querySelector('#user_id').textContent = user_id;
+        // document.querySelector('#username').innerText = username;
+        // document.querySelector('#lang').innerText = user_language;
+    }
 });
 
 
